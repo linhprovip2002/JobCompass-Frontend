c2b7ac05d93e1afd42c625af6468d2d1fad261d4
Quannisme <121941593+Quannisme@users.noreply.github.com>
2025-02-05 11:29:39 +0700

    fix: restyle responsive

--- src/components/custom-ui/form-email-verify.tsx
+++ src/components/custom-ui/form-email-verify.tsx
@@ -5,9 +5,9 @@ import { LuArrowRight } from 'react-icons/lu';
 
 export function FormEmailVerify() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
-            <div className="w-full max-w-md flex flex-col items-center justify-center">
-                <div className="space-y-6 text-center">
+        <form className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
+            <div className="flex flex-col items-center justify-center">
+                <div className="max-w-[536px] space-y-9 text-center px-5 md:px-0">
                     <h1 className="text-[32px] leading-[40px] font-inter font-medium">Email Verification</h1>
 
                     <p className="font-inter text-base leading-6 text-muted-foreground">
@@ -16,22 +16,26 @@ export function FormEmailVerify() {
                         address and activate your account.
                     </p>
 
-                    <div className="space-y-4">
-                        <Input type="text" placeholder="Verification Code" className="w-[536px] h-[64px]" />
-
-                        <Button
-                            type="submit"
-                            className="group h-14 w-[536px] rounded-sm text-base [&_svg]:size-6 font-semibold"
-                        >
-                            Verify My Account <LuArrowRight className="group-hover:translate-x-2 transition-all" />
-                        </Button>
+                    <div>
+                        <Input
+                            name="code"
+                            placeholder="Verification Code"
+                            type="text"
+                            className="h-16 rounded-sm focus-visible:border-primary focus-visible:ring-primary"
+                        />
                     </div>
+                    <Button
+                        type="submit"
+                        className="group w-full h-14 rounded-sm text-base [&_svg]:size-6 font-semibold"
+                    >
+                        Verify My Account <LuArrowRight className="group-hover:translate-x-2 transition-all" />
+                    </Button>
 
                     <p className="text-sm">
                         Didn&apos;t receive any code? <button className="text-blue-600 hover:underline">Resend</button>
                     </p>
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
