5c12cec59bc17dc2209b6545b1ca3673040ba720
Quannisme <121941593+Quannisme@users.noreply.github.com>
2025-02-10 22:23:12 +0700

    update: api verify email

--- src/app/(content-only-layout)/(only-center-logo)/email-verify/page.tsx
+++ src/app/(content-only-layout)/(only-center-logo)/email-verify/page.tsx
@@ -2,7 +2,16 @@
 import { ButtonHome } from '@/components/custom-ui/button-home';
 import { FormEmailVerify } from '@/components/custom-ui/form-email-verify';
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 
 export default function EmailVerify() {
-    return <FormEmailVerify />;
+    return (
+        <div>
+            <div>
+                <ToastContainer position="top-right" autoClose={3000} />
+                <FormEmailVerify />
+            </div>
+            ;
+        </div>
+    );
 }
