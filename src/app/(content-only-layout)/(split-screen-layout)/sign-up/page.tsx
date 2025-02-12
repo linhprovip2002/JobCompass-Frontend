6716407dc8906f409e8e70400928fd8b2ffc3b45
Quannisme <quanismevn@gmail.com>
2025-02-12 17:12:41 +0700

    update: update api auth

--- src/app/(content-only-layout)/(split-screen-layout)/sign-up/page.tsx
+++ src/app/(content-only-layout)/(split-screen-layout)/sign-up/page.tsx
@@ -1,10 +1,8 @@
 import { FormSignUp } from '@/components/custom-ui/form-sign-up';
-import { ToastContainer } from 'react-toastify';
 
 export default function SignUp() {
     return (
         <div>
-            <ToastContainer position="top-right" autoClose={3000} />
             <FormSignUp />
         </div>
     );
