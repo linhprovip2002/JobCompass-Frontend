13e5aec406ac55ef050148fa1994e785fcca15d6
Quannisme <quanismevn@gmail.com>
2025-02-12 09:15:50 +0700

    update: merge dev

--- src/app/(content-only-layout)/(split-screen-layout)/sign-up/page.tsx
+++ src/app/(content-only-layout)/(split-screen-layout)/sign-up/page.tsx
@@ -1,8 +1,10 @@
 import { FormSignUp } from '@/components/custom-ui/form-sign-up';
+import { ToastContainer } from 'react-toastify';
 
 export default function SignUp() {
     return (
         <div>
+            <ToastContainer position="top-right" autoClose={3000} />
             <FormSignUp />
         </div>
     );
