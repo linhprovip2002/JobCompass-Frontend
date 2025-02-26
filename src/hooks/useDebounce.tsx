17c7c5c19e77e79cee047ae0ea7e14fa3778592f
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-02-26 16:12:12 +0700

    chore: add useDebounce hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+/**
+ * How to use: const debouncedSearch = useDebounce(search, 1000)
+ * @param value : string - the text to be debounced
+ * @param delay : ms - the time to delay the debounced
+ * @returns debouncedValue: string - the debounced value
+ */
+export function useDebounce(value: string, delay: number) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setDebouncedValue(value), delay);
+        return () => clearTimeout(timer);
+    }, [value, delay]);
+
+    return debouncedValue;
+}
