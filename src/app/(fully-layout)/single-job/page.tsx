5800a99f0247f40fb10b294c0931a69ecf72383c
Quannisme <quanismevn@gmail.com>
2025-03-03 12:09:35 +0700

    feat: remove debounce

--- src/app/(fully-layout)/single-job/page.tsx
+++ src/app/(fully-layout)/single-job/page.tsx
@@ -24,7 +24,6 @@ import { useSearchParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { queryKey } from '@/lib/react-query/keys';
 import { JobService } from '@/services/job.service';
-import moment from 'moment';
 import DOMPurify from 'dompurify';
 import { Suspense } from 'react';
 
